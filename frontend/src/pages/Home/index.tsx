74d636d81ab5be780de1ddbe7307a14720d9aa46
Humberto Junior <astronomi@gmail.com>
2024-06-06 14:51:01 -0300

    Update screen home

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,3 +1,5 @@
+import { useCallback, useState } from "react";
+
 import Header from "../../components/Header/Header";
 import Container from "../../components/Container/Container";
 import Title from "../../components/Title/Title";
@@ -14,15 +16,50 @@ const genderBooks = [
 ];
 
 export default function Home() {
+  const [selectedGender, setSelectedGender] = useState<string[]>([]);
+
+  const handleSelect = useCallback(
+    (title: string) => {
+      console.log("==> home: ", title);
+      if (selectedGender.includes(title)) {
+        const removeGender = selectedGender.filter((item) => item !== title);
+        setSelectedGender(removeGender);
+      } else {
+        setSelectedGender([...selectedGender, title]);
+      }
+    },
+    [selectedGender]
+  );
+
   return (
     <>
       <Header />
       <Container>
         <Title title="O que você quer ler hoje?" />
 
-        {genderBooks.map((book) => (
-          <Button title={book} variant="light" />
-        ))}
+        <div className="gap-8 grid grid-cols-8 my-6">
+          {genderBooks.map((book) => (
+            <Button
+              title={book}
+              variant={selectedGender.includes(book) ? "dark" : "light"}
+              onClick={() => handleSelect(book)}
+            />
+          ))}
+        </div>
+
+        <div className="py-7">
+          <p className="text-evergreen font-semibold text-2xl">
+            Sobre o que você gostaria de receber uma recomendação de livro?
+          </p>
+
+          <input
+            type="text"
+            placeholder="Eu gostaria de ler..."
+            className="outline-none shadow-lg border border-gray-100 rounded-3xl w-full px-6 py-3 mt-3"
+          />
+        </div>
+
+        <Title title="Livros recomendados" />
       </Container>
     </>
   );
