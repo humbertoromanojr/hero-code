b88b8ce7c4c0bee7aa617970c656e3a792b12d73
Humberto Junior <astronomi@gmail.com>
2024-06-06 17:00:37 -0300

    Create screen BookDetails

---
+++ frontend/src/pages/BookDetails/index.tsx
@@ -0,0 +1,10 @@
+import Container from "../../components/Container/Container";
+import HeaderTitle from "../../components/Header/HeaderTitle";
+
+export default function componentName() {
+  return (
+    <Container>
+      <HeaderTitle />
+    </Container>
+  );
+}
