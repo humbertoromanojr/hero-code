573dbebb932c31b8bf2a4020de3613c9e2d7ea88
Humberto Junior <astronomi@gmail.com>
2024-06-06 16:28:20 -0300

    Finished component Tag

--- frontend/src/components/Tag/Tag.tsx
+++ frontend/src/components/Tag/Tag.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import { useCallback } from "react";
 
-export default function Tag() {
-  return <div></div>;
+type Props = {
+  title: string;
+};
+
+export default function Tag({ title }: Props) {
+  const randomColors = useCallback(() => {
+    const hue = Math.floor(Math.random() * 360);
+    const lightness = `hsl(${hue}, 100%, 95%)`;
+    const darkness = `hsl(${hue}, 100%, 20%)`;
+
+    return { lightness, darkness };
+  }, []);
+
+  return (
+    <p
+      style={{
+        backgroundColor: randomColors().lightness,
+        color: randomColors().darkness,
+      }}
+      className="rounded-full bg-opacity-50 px-3 text-center w-fit py-2"
+    >
+      {title}
+    </p>
+  );
 }
