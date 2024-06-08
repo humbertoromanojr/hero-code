1950e968f57bfb9ccf8a14aa949445b779f2b449
Humberto Junior <astronomi@gmail.com>
2024-06-08 15:04:49 -0300

    Create BookRoutes.ts

---
+++ backend/src/infra/router/booksRoutes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+import { BooksController } from "../../app/controllers/books.controller";
+
+export const BookRoutes = (router: Router) => {
+  const booksController = new BooksController();
+
+  router.post("/books", booksController.create);
+};
