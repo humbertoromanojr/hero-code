bd7d1f9458f7d52757ad3b12af8d2d0f2988661d
Humberto Junior <astronomi@gmail.com>
2024-06-18 11:30:10 -0300

    Update bookAdapter.ts missing routerAdapter

--- backend/src/infra/router/booksRoutes.ts
+++ backend/src/infra/router/booksRoutes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { BooksController } from "../../app/controllers/books.controller";
+import { routerAdapter } from "./routerAdapter";
 
 export const BookRoutes = (router: Router) => {
   const booksController = new BooksController();
